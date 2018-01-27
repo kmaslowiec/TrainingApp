9d9d653ba48a9e03f54782069b8f177257c9121d
Kondzik <kmaslowiec@gmail.com>
2018-01-27 12:41:51 +0100

    version 0.3.1 JAVA
    *1t version of timer implemented to the TextView Left Top Corner

--- app/src/main/java/com/example/android/trainwithme/MainActivity.java
+++ app/src/main/java/com/example/android/trainwithme/MainActivity.java
@@ -1,25 +1,80 @@
 package com.example.android.trainwithme;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // below hides the status bar but the class has to be changed to Activity !!!
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    public void timer(View view) {
+
+        txt = (TextView) findViewById(R.id.timer2);
+
+        txt.setText("Test");
+
+        final CounterClass time = new CounterClass(30000  , 1000);
+
+        time.start();
 
     }
 
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    @SuppressLint("NewApi")
+    public class CounterClass extends CountDownTimer {
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
 
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @SuppressLint("NewApi")
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+
+            long millis = millisUntilFinished;
+
+
+            String hms = String.format(Locale.CANADA, "%02d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes(millis)
+                            - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+
+
+            System.out.println(hms);
+            txt.setText(hms);
+        }
+
+        @Override
+        public void onFinish() {
+
+            txt.setText("Complete");
+
+        }
+
+
+    }
 }
